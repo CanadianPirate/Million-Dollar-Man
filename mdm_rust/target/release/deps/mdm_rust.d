4a4137216c4ec6c3249aa29dadcba9756607e94d
Chandler Dionne <>
2020-12-12 00:12:57 -0500

    Struct Fix

--- mdm_rust/target/release/deps/mdm_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\chand\Documents\GitHub\Million-Dollar-Man\mdm_rust\target\release\deps\mdm_rust.exe: src\main.rs
-
-C:\Users\chand\Documents\GitHub\Million-Dollar-Man\mdm_rust\target\release\deps\mdm_rust.d: src\main.rs
-
-src\main.rs:
--- mdm_rust/target/release/mdm_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\chand\Documents\GitHub\Million-Dollar-Man\mdm_rust\target\release\mdm_rust.exe: C:\Users\chand\Documents\GitHub\Million-Dollar-Man\mdm_rust\src\main.rs
