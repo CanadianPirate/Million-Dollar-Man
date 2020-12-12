e2aaaff8013b63e0a29943512a73e00167233986
Chandler Dionne <>
2020-12-12 18:23:02 -0500

    Added the rest of the functionality for Python & Rust

--- mdm_py/MDM.py
+++ mdm_py/MDM.py
@@ -1,13 +1,35 @@
-""" Additon list
-Vars: starting cash, expected interest, years of return, quarters/montly increments, quarterly/montly addiditons
+def Calc_interest(principal, increment, interest, addition, price_growth, div_growth):
+    years = 0
+    interest /= 100
+    interest /= increment
+    price_growth /= 100
+    div_growth /= 100
+    while (principal < 1000000):
+        for i in range(increment):
+            principal += principal * price_growth
+            principal += addition
+            principal += principal * interest
+        interest *= div_growth + 1
+        years += 1
+    return years
 
-"""
 
-def Calc_interest(principal, increments, interest):
-    for i in range(increments):
-        principal *= ((interest / 100) + 1)
-
-    return principal
 
 if __name__ == "__main__":
-    print(Calc_interest(1000, 4, 10))
+
+    # Getting data
+    assets = float(input("How much cash/assets do you have currently? "))
+    interest = float(input("How much interest can you expect to make on the assets you have? (Annual percent as a whole number) "))
+    increment = input("How often do you think you will get an investment return? (M or Q) ")
+    addition = float(input("How much do you think you will be able to add to your assets in each Month/Quarter? "))
+    price_growth = float(input("How much do you expect your stocks to inflate in price per year? (Annual percent as a whole number) "))
+    div_growth = float(input("Lastly, How much do you expect your dividend rates to grow per year? (Annual Percent as a whole number)"))
+
+    # Checking increment value
+    if increment == "m" or increment == "M":
+        increments = 12
+    elif increment == "q" or increment == "Q":
+        increment = 4
+    else:
+        pass
+    print("It will take you", Calc_interest(assets, increment, interest, addition, price_growth, div_growth), "year(s) to be a million dollar man!")
--- mdm_rust/src/main.rs
+++ mdm_rust/src/main.rs
@@ -1,10 +1,48 @@
-fn calc_interest(mut principal: f64, increments: u32, interest: f64) -> f64{
-    for _ in 0..increments {
-        principal = principal * ((interest / 100.0) + 1.0);
+use text_io::read;
+
+// Function to check compounding interest
+fn calc_interest(mut principal: f64, increment: u32, mut interest: f64, addition: f64, mut price_growth: f64, mut div_growth: f64) -> f64 {
+    let mut years: f64 = 0.0;
+    interest /= 100.0;
+    interest /= increment as f64;
+    price_growth /= 100.0;
+    div_growth /= 100.0;
+    while principal < 1000000.0{
+        for _ in 0..increment {
+            principal += principal * price_growth;
+            principal += addition;
+            principal += interest * principal;
+        }
+        interest *= div_growth + 1.0;
+        years += 1.0;
     }
-    return principal
+    return years
 }
 
 fn main() {
-    println!("Your final amount is {}", calc_interest(1000.0, 4, 10.0));
+
+    // Getting data
+    println!("How much cash/assets do you have currently? ");
+    let assets: f64 = read!();
+    println!("How much interest can you expect to make on the assets you have? (Annual percent as a whole number) ");
+    let interest: f64 = read!();
+    println!("How often do you think you will get an investment return? (M or Q) ");
+    let increment_type: String = read!();
+    println!("How much do you think you will be able to add to your assets in each Month/Quarter? ");
+    let addition: f64 = read!();
+    println!("How much do you expect your stocks to inflate in price per year? (Annual percent as a whole number) ");
+    let price_growth: f64 = read!();
+    println!("Lastly, How much do you expect your dividend rates to grow per year? (Annual Percent as a whole number)");
+    let div_growth: f64 = read!();
+    let mut increment: u32 = 4;
+
+    // Checking Increment
+    if increment_type == "m" || increment_type == "M" {
+        increment = 12;
+    }
+
+
+    println!("It will take you {} year(s) to be a million dollar man.", calc_interest(assets, increment, interest, addition, price_growth, div_growth));
+
+
 }
